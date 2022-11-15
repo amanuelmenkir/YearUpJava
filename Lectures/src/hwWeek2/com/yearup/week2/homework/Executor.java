2404042bae87d5e94a877625c4e5c31645b0ebce
Amanuel Menkir <amanuelmenkir16@gmail.com>
2022-11-14 23:29:34 -0500

    added week 2 homework

---
+++ Lectures/src/hwWeek2/com/yearup/week2/homework/Executor.java
@@ -0,0 +1,15 @@
+package com.yearup.week2.homework;
+import java.util.Scanner;
+
+
+public class Executor {
+
+    static Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[]args){
+
+    }
+
+
+}
---
+++ Lectures/src/hwWeek2/com/yearup/week2/homework/Planet.java
@@ -0,0 +1,34 @@
+package com.yearup.week2.homework;
+
+
+public class Planet implements Calculator{
+
+    protected String myStar;
+    private String planetName;
+    private double planetRadius;
+
+    public String getMyStar(){
+        return this.myStar;
+    }
+
+
+    public static void main(String[]arg){
+
+    }
+
+    @Override
+    public double calculateArea(double length, double width) {
+        this.planetRadius = length * width * 3.14;
+        return 0;
+    }
+
+    @Override
+    public double calculateSum(double value1, double value2) {
+        return 0;
+    }
+
+    @Override
+    public double calculateVolume(double length, double width, double height) {
+        return 0;
+    }
+}
